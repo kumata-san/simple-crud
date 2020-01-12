c1a54a1bc8784f2c8af482dcbe7f3b942cb0bb7c
kumata-san <kumatasan2019@gmail.com>
2020-01-13 01:58:34 +0900

    change reducers.js

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,10 +1,32 @@
+import { combineReducers } from 'redux'
+import { CHANGE_NAME, CHANGE_AGE, INITIALIZE_FORM } from './actions'
+
 const initialState = {
-    form: {
+    form: { // 今回はControlledとして扱う
         name: '',
         age: ''
     },
-    characters: {
+    characters: { // isFetching === trueのときにLoading...という風に出したい。アプリが現状把握するために必要
         isFetching: false,
         characterArray: []
+    }
+}
+
+const formReducer = (state = initialState.form, action) => {
+    switch (action.type) {
+        case CHANGE_NAME:
+            return {
+                ...state,
+                name: action.name
+            }
+        case CHANGE_AGE:
+            return {
+                ...state,
+                age: action.age
+            }
+        case INITIALIZE_FORM:
+            return initialState.form
+        default:
+            return state
     }
 }
