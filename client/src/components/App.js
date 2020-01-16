3c687ba8f0a8dc7c6dc397b61a6ebc70f1e68bd6
kumata-san <kumatasan2019@gmail.com>
2020-01-16 22:49:20 +0900

    rename AppForm.js to AddForm.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render(){
       return (
         <div>
-          <AppForm store={this.props.store} />
+          <AddForm store={this.props.store} />
           <CharacterList store={this.props.store} />
         </div>
       )
