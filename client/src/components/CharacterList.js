c895f38ee973384b22b06fbb674643ffe2fbcdea
kumata-san <kumatasan2019@gmail.com>
2020-01-19 00:21:53 +0900

    add code into CharacterList.js, actions.js and reducers.js

--- client/src/components/CharacterList.js
+++ client/src/components/CharacterList.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { requestData, receiveDataSuccess, receiveDataFailed } from "../actions";
+import axios from 'axios';
 
 const CharacterList = ({ store }) => {
+    const { isFetching, characterArray } = store.getState().characters
+
+    const handleFetchData = () => {
+        store.dispatch(requestData())
+        axios.get('/api/characters')
+        .then(response => {
+            const _characterArray = response.data
+            store.dispatch(receiveDataSuccess(_characterArray))
+        })
+        .catch(err => {
+            console.error(new Error(err))
+            store.dispatch(receiveDataFailed())
+        })
+    }
     return (
         <div>
-
+            {
+                isFetching
+                ? <h2>Now Loading...</h2>
+                : <div>
+                    <button onClick={() => handleFetchData()}>fetch data</button>
+                    <ul>
+                        {characterArray.map(character => (
+                            <li key={character._id}>
+                                {`${character.name}(${character.age})`}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            }
         </div>
     )
 }
